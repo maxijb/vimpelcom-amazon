104c731c632e43f0c9296f382a36377b4432f611
mbenedetto <echeverria@yahoo.com.ar>
2016-03-31 19:30:22 -0300

    adding postgre

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var mongoose = require('mongoose'); 
+var pg = require('pg');
 var database = {
 
 	// the database url to connect
-	url : 'mongodb://mbenedetto:vimpel1@ec2-54-209-128-66.compute-1.amazonaws.com:27017/dummyDB'
+	url : 'mongodb://mbenedetto:vimpel1@ec2-54-209-128-66.compute-1.amazonaws.com:27017/dummyDB',
+	postgre: 'postgres://vimpel.cyemgufihio8.us-east-1.rds.amazonaws.com:5432'
 };
 
 
 mongoose.connect(database.url);
+
+var client = new pg.Client(database.postgre);
+client.connect();
+
 
 var app = express();
 app.engine('jade', require('jade').__express)
