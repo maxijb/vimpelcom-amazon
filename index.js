45a81dc02e59e1387a32c263a514e04343f2a2de
mbenedetto <echeverria@yahoo.com.ar>
2016-03-31 18:41:25 -0300

    adding mongo

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var express = require('express');
+var mongoose = require('mongoose'); 
+var database = {
+
+	// the database url to connect
+	url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'
+};
+
+
+mongoose.connect(database.url);
 
 var app = express();
 app.engine('jade', require('jade').__express)
